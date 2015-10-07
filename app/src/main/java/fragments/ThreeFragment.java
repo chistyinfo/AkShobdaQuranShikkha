04fca1319b06a1334f62fc62513ec234aa7dd690
chisty <chistyinfo@gmail.com>
2015-10-07 08:38:56 +0600

    five new tab ...and package name is corrected

--- app/src/main/java/fragments/ThreeFragment.java
+++ app/src/main/java/fragments/ThreeFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import akshobdayquaran.R;
+import theoaktroop.akshobdaquranshikkha.R;
 
 
 public class ThreeFragment extends Fragment{

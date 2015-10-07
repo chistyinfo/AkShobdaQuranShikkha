7fcf2cc55f49134e1610767c7a19943abceba21d
hasan_cse91 <hasan_cse91@yahoo.com>
2015-10-07 22:30:16 +0600

    WebView Add

--- app/src/main/java/fragments/TwoFragment.java
+++ app/src/main/java/fragments/TwoFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import theoaktroop.akshobdaquranshikkha.R;
 
@@ -18,13 +20,43 @@ public class TwoFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_two, container, false);
+
+        WebView webView = (WebView) v.findViewById(R.id.bookWebView);
+
+
+
+        String  url;
+
+        url = "http://afrozaramzanhighschool.edu.bd/";
+
+
+        webView.setWebViewClient(new MyBrowser());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+//            webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+
         return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+
+            return true;
+        }
     }
 
 }

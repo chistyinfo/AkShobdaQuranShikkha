4187d31c5882f745c01bd7c4fd763d7d72acd7a4
hasan_cse91 <hasan_cse91@yahoo.com>
2015-10-15 23:44:46 +0600

    Developer info and VDO are OK

--- app/src/main/java/fragments/ThreeFragment.java
+++ app/src/main/java/fragments/ThreeFragment.java
@@ -5,10 +5,8 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.session.MediaController;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -16,9 +14,6 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.VideoView;
 
 import java.io.IOException;
 
@@ -29,14 +24,13 @@ public class ThreeFragment
         extends Fragment
         implements SurfaceHolder.Callback {
 
-    Uri targetUri = Uri.parse("android.resource://" + R.raw.small);
+    Uri targetUri = Uri.parse("android.resource://"+"theoaktroop.akshobdaquranshikkha"+ "/"  + R.raw.small);
 
     MediaPlayer mediaPlayer;
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     boolean pausing = false;
 
-    TextView mediaUri;
     Button buttonPlayVideo, buttonPauseVideo;
     private  Context context;
     public ThreeFragment() {
@@ -55,7 +49,7 @@ public class ThreeFragment
 
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_three,container,false);
-        mediaUri = (TextView)view.findViewById(R.id.mediauri);
+
         buttonPlayVideo = (Button)view.findViewById(R.id.playvideoplayer);
         buttonPauseVideo = (Button)view.findViewById(R.id.pausevideoplayer);
         surfaceView = (SurfaceView)view.findViewById(R.id.surfaceview);
@@ -89,13 +83,13 @@ public class ThreeFragment
                         mediaPlayer.prepare();
                     } catch (IllegalArgumentException e) {
                         e.printStackTrace();
-                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+//                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
                     } catch (IllegalStateException e) {
                         e.printStackTrace();
-                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+//                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
                     } catch (IOException e) {
                         e.printStackTrace();
-                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+//                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
                     }
 
                     mediaPlayer.start();
